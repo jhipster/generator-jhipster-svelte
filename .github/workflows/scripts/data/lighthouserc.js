1b24d1deddf2686e71a030a808504657fef4497c
Vishal Mahajan <vishal423@gmail.com>
2021-04-15 23:56:02 +0530

    feat: Integrate Lighthouse CI

---
+++ .github/workflows/scripts/data/lighthouserc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	ci: {
+		assert: {
+			preset: 'lighthouse:recommended',
+		},
+		collect: {
+			url: ['http://localhost:8080/'],
+			startServerCommand: 'docker-compose -f src/main/docker/app.yml up -d',
+			startServerReadyTimeout: 120000,
+		},
+		upload: {
+			target: 'temporary-public-storage',
+		},
+	},
+};
