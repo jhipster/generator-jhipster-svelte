247e7c16512da1339a4ba59c623c5270b5d60299
Vishal Mahajan <vishal423@gmail.com>
2021-04-17 21:25:03 +0530

    Wait for selector to continue

--- .github/workflows/scripts/lighthouse/puppeteer-auth.js
+++ .github/workflows/scripts/lighthouse/puppeteer-auth.js
@@ -1,6 +1,7 @@
 module.exports = async (browser, context) => {
 	const page = await browser.newPage();
 	await page.goto('http://localhost:8080/login');
+	await page.waitForSelector('#username', { timeout: 4000 });
 	await page.type('#username', 'admin');
 	await page.type('#password', 'admin');
 	await page.click('[type="submit"]');
