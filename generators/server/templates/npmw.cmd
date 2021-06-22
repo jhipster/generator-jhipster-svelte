e62ab58f462099a62bab98e88272984a934dcfc4
Vishal Mahajan <vishal423@gmail.com>
2021-06-22 19:55:23 +0530

    Override with local npmw and handle gradle flows

---
+++ generators/server/templates/npmw.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+@setlocal
+
+set NPMW_DIR=%~dp0
+
+if exist "%NPMW_DIR%\mvnw.cmd" (
+  set NPM_EXE=%NPMW_DIR%\target\node\npm.cmd
+  set INSTALL_NPM_COMMAND=%NPMW_DIR%\mvnw.cmd frontend:install-node-and-npm@install-node-and-npm
+)
+
+if not exist %NPM_EXE% (
+  call %INSTALL_NPM_COMMAND%
+)
+
+if not exist %NPM_EXE% goto globalNpm
+
+%NPM_EXE% %*
+
+:globalNpm
+npm %*
