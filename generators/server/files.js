16558a34e242f6e932e91d5aa2347863ebd6f335
Vishal Mahajan <vishal423@gmail.com>
2022-09-09 17:57:02 +0530

    chore: Remove reactive overrides in blueprint

--- generators/server/files.js
+++ generators/server/files.js
@@ -14,28 +14,6 @@ const commonFiles = {
 			templates: ['src/main/resources/banner.txt'],
 		},
 	],
-	serverJavaAuthConfig: [
-		{
-			condition: generator => generator.reactive,
-			path: SERVER_MAIN_SRC_DIR,
-			templates: [
-				{
-					file: 'package/config/SecurityConfiguration_reactive.java',
-					renameTo: generator => `${generator.javaDir}config/SecurityConfiguration.java`,
-				},
-			],
-		},
-		{
-			condition: generator => !generator.skipUserManagement,
-			path: SERVER_MAIN_SRC_DIR,
-			templates: [
-				{
-					file: 'package/web/rest/UserResource.java',
-					renameTo: generator => `${generator.javaDir}web/rest/UserResource.java`,
-				},
-			],
-		},
-	],
 };
 
 function writeFiles() {
